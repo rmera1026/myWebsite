03ec2ae304c03626e20ddee5760e40688f7f182e
Ronald Mera <roncollege1026@gmail.com>
2022-01-25 18:08:13 -0500

    projects update

--- js/scripts.js
+++ js/scripts.js
@@ -52,7 +52,7 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
     // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
+    // new SimpleLightbox({
+    //     elements: '#portfolio a.portfolio-box'
+    // });
 });
